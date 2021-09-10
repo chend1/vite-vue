238c3727e4e0a3406d19946493f7976c70fc0db2
chendong <chendong@ncyymt.com>
2021-09-10 10:34:22 +0800

    初始化项目

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="@/assets/logo.png" />
+    <div class="home">{{num}}</div>
+    <div>d1222223</div>
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+    <div class="change" @click="changeNum(100)">
+      改变num的值
+    </div>
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from '@vue/reactivity'
+let num = ref(122111211)
+function changeNum(id){
+  console.log(id);
+  num.value+=id
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
